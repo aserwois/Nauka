c0a1ca877421c21b918ba1f98b532bcaec5aadbd
aserwois <mateusz.gabriel90@gmail.com>
2020-01-01 19:00:27 +0100

    Age Program

--- Program.cs
+++ Program.cs
@@ -6,8 +6,24 @@ namespace Hello
     {
         static void Main(string[] args)
         {
-            string name = "Mati";
-            Console.WriteLine("Witaj mistrzu" + name);
+            Console.WriteLine("Wpisz swoje Imie: ");
+            string name = Console.ReadLine();
+
+            Console.WriteLine("Witaj, " + name);
+
+            Console.WriteLine("Wpisz ile masz lat: ");
+            int age = int.Parse(Console.ReadLine());
+
+            if(age >= 18)
+            {
+                Console.WriteLine("Brawo, jesteś pełnoletni, walnij se browara!");
+            } 
+            else 
+            {
+                Console.WriteLine("To wypad!");
+            }
+
+            Console.ReadKey();
         }
     }
 }
