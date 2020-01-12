4957b22bca25c2e633580377aa97f511aa958b0c
aserwois <mateusz.gabriel90@gmail.com>
2020-01-12 12:11:13 +0100

    guess1

--- guess.cs
+++ guess.cs
@@ -8,16 +8,18 @@ namespace guessProgram
         {
             string password = "batman";
             string guess = "";
+            int guessCounter = 0;
 
-            while(guess != password)
+            do
             {
             System.Console.Write("Enter the guess: ");
             guess = Console.ReadLine();
+            guessCounter++;
             }
+            while(guess != password);
 
             System.Console.WriteLine("You won!");
+            System.Console.WriteLine($"You was trying to guest {guessCounter} times!");
         }
-
-
     }
 }
